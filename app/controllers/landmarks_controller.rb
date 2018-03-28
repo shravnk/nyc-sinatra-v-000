f517ee3be25dbc14d9f0ce23713bcd8ec8be6a3a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-28 19:57:47 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,48 @@
 class LandmarksController < ApplicationController
-  get '/landmarks/new' do
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
   end
+
+  get '/landmarks' do
+    @landmarks = landmark.all
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do
+    @landmark = landmark.create(params[:landmark])
+
+    @landmark.titles.create(params[:title]) unless params[:title][:name].empty?
+    @landmark.landmarks.create(params[:landmark]) unless params[:landmark][:name].empty?
+    @landmark.save
+
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+
+    @landmark = landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+
+    if !params[:title][:name].empty?
+      @landmark.titles << Title.create(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @landmark.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+    @landmark.save
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+
 end
