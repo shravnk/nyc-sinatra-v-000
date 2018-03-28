7b4799aec5ab36dbbdc7db8ec5f21beec9260951
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-28 20:27:08 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -25,7 +25,25 @@ class FiguresController < ApplicationController
     erb :'figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
 
+  patch '/figures/:id' do
+
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+    @figure.save
+    redirect "figures/#{@figure.id}"
+  end
 
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,40 @@
 class LandmarksController < ApplicationController
-  get '/landmarks/new' do
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
   end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+
+    @landmark.save
+
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+
+    @landmark.save
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+
 end
