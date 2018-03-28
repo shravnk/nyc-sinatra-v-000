b8e97f5e3b32ba72d2a606d38b852d3a05b6068a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-28 18:16:14 +0000

    New section complete

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,9 +1,31 @@
+require 'pry'
 class FiguresController < ApplicationController
 
   get '/figures/new' do
-    @figures = Figure.all
     erb :'/figures/new'
   end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    @figure.titles.create(params[:title]) unless params[:title][:name].empty?
+    @figure.landmarks.create(params[:landmark]) unless params[:landmark][:name].empty?
+    @figure.save
+
+    redirect "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+
 
 
 end
