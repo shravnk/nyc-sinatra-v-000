4ef27b89278b450d4df4acc37879cb30ecc1e9e2
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-28 19:46:18 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -31,7 +31,7 @@ class FiguresController < ApplicationController
   end
 
   patch '/figures/:id' do
-    binding.pry
+    
     @figure = Figure.find_by_id(params[:id])
     @figure.update(params[:figure])
 
